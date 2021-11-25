b485a1eb1aff6de607940f2fdd3cac7187a06975
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-11-25 22:18:49 +0530

    type-start

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,31 @@ import "./App.css";
 function App() {
   // let firstValue: string = "Many";
   // let firstValue: number = 5;
-  let firstValue: boolean = true;
+  // let firstValue: boolean = true;
+  // let firstValue: number[] = [2, 3];
+  // let firstValue: Array<string> = ["232", "23232", "sfasf"];
+
+  // Tuple
+  let aTuple: [string, number] = ["dwd", 23];
+
+  enum Codes {
+    first = 1,
+    second = 2,
+  }
+
+  let firstName: any = "Manny";
+  // Void
+
+  const warnings = (): void => {
+    console.log("warning");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          The Value {firstValue} is of {typeof firstValue} type!
+          The Value {firstName} is of {typeof firstName} type!
         </p>
       </header>
     </div>
